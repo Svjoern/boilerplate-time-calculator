3fe0ee94ceaee159cac53005bdb036f24d4c4bd5
Sven <Sven_Kuehne@hotmail.de>
2022-02-09 05:22:36 +0000

    jk

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from time_calculator import add_time
 from unittest import main
 
 
-print(add_time("11:55 PM", "48:20"))
-
+#print(add_time("11:55 PM", "48:20"))
+# print(add_time("3:00 PM", "3:10"))
+# print(add_time("11:30 AM", "2:32", "Monday"))
+# print(add_time("11:43 AM", "00:20"))
+# print(add_time("10:10 PM", "3:30"))
+print(add_time("11:43 PM", "24:20", "tueSday"))
+# print(add_time("6:30 PM", "205:12"))
 
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
+
+print(add_time("11:55 AM", "3:12"))
+
+# 6:10 PM
+# 2:02 PM, MONDAY
+# 12:03 AM
+# 1:40 AM (next day)
+# 0:03 AM, THURSDAY (3 days later)
+# 7:42 AM (9 days later)
--- time_calculator.py
+++ time_calculator.py
@@ -1,37 +1,42 @@
-Weekdays = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
+Weekdays = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
 
 def add_time(start, duration, dayofWeek=""):
+  print("start\t\t\t\t",start,duration,dayofWeek)
+  hours_over=0
   days = 0
   day_index = 0
   if dayofWeek!= "":
-    day_index = Weekdays.index(dayofWeek.upper())
-    print(day_index, "test")
+    day_index = Weekdays.index(dayofWeek.lower())
+    # print(day_index, "day_index")
   
-  print("start\t\t\t\t",start,duration,dayofWeek)
-  
-  times = start[-2:]
+  times = start.split(" ")[1]
+  hours = int(start.split(":")[0])
   if times == "PM":
-    hours = 12 + int(start[0:2])
-  elif times == "AM":
-    hours = 0 + int(start[0:2])
-
-  minutes = int(start[-5:-3]) + int(duration[-2:])
-
-  print("hours-transform\t\t",hours,":", minutes, times, days, "days")
-
+    hours += 12
+  # print("hours-transform\t\t",hours,":", times, days, "days")
+  
   minutes = int(start[-5:-3]) + int(duration[-2:])
   if minutes > 60:
     minutes -= 60
     hours_over = 1
-  print("minutes-transform\t",hours+hours_over,":", minutes, times, days, "days")
 
-  hours += int(duration.split(":")[0]) + hours_over
-  print("hours-added\t\t\t",hours+hours_over,":", minutes, times, days, "days")
+  if minutes < 10:
+    minutes = "0"+str(minutes)
+  else: 
+    minutes = str(minutes)
 
-  while hours > 24:
+  # print("minutes-transform\t",hours+hours_over,":", minutes, times, days, "days")
+
+  hours_chg = int(duration.split(":")[0]) + hours_over
+  print("hours-added\t\t\t",hours+hours_chg,":", minutes, times, days, "days")
+
+  hours = hours+hours_chg
+
+  while hours >= 24:
     days += 1
     hours -= 24
-  if hours > 12:
+    print("here")
+  if hours >= 13:
     times = "PM"
     hours -= 12
   else:
@@ -39,14 +44,25 @@ def add_time(start, duration, dayofWeek=""):
   
   if dayofWeek!= "":
     days = (day_index + days)%7
-  else:
-    if days == 1:
-      days = "(next day)"
-      print("days", days, Weekdays[days])
+    day_name = Weekdays[days][0].upper()+Weekdays[days][1:]
+    if days == 0:
+      days = ", " + day_name
+    elif days == 1:
+      days = ", " + day_name + " (next day)"
+      # print("days", days, Weekdays[days])
     else:
-      days = "( " + str(days) + "days later)"
-      print("days", days)
-  print("days-transform\t\t",hours,":", minutes, times, days)
+      days = ", " + day_name + " (" + str(days) + " days later)"
+      # print("days", days)  
+  else:
+    if days == 0:
+      days = ""
+    elif days == 1:
+      days = " (next day)"
+      # print("days", days, Weekdays[days])
+    else:
+      days = " (" + str(days) + " days later)"
+      # print("days", days)
+  # print("days-transform\t\t",hours,":", minutes, times, days)
  
 
-  return str(hours) + ":" + str(minutes) + " " + times + " " + str(days) + "days"
+  return str(hours) + ":" + minutes + " " + times + str(days)
